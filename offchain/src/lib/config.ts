941e934d05307474165c154e921e582e99440909
Emmanuel .A <asaoluemma@gmail.com>
2025-12-12 03:54:23 +0100

    update: offchain code

---
+++ offchain/src/lib/config.ts
@@ -0,0 +1,8 @@
+import { stringToHex } from "@meshsdk/core"
+
+const admin_token_policy = "a8d770ae253e4818feb0a5f55dc29d85d86061feee7cc31347276322"
+const admin_token_name = stringToHex("aidPod-admin")
+export const admin_token = {
+    policy: admin_token_policy,
+    name: admin_token_name
+};
