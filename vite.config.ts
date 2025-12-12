20f8ec22f7a2ef4e268fd95437e2f672c4a48da3
Emmanuel .A <asaoluemma@gmail.com>
2025-12-12 15:05:30 +0100

    fix: bug of page viewer

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,28 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
-      external: ['readable-stream']
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
+              return 'react-vendor';
+            }
+            if (id.includes('@meshsdk')) {
+              return 'mesh-vendor';
+            }
+            if (id.includes('@react-three') || id.includes('three')) {
+              return 'three-vendor';
+            }
+            // Everything else goes to vendor chunk
+            return 'vendor';
+          }
+        }
+      }
     }
   },
   optimizeDeps: {
@@ -31,6 +48,10 @@ export default defineConfig({
         process: true,
       },
       protocolImports: true,
+      include: ['stream'],
+      overrides: {
+        'readable-stream': 'stream-browserify',
+      },
     }),
   ],
   server: {
